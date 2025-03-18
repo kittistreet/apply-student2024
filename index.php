ec8da78834a98ddd9d290672e9e777095adfbc55
Kittithuch <kittistreet.th@gmail.com>
2025-03-18 08:48:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require 'connect-pdo.php';
 session_start();
 if (empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-    echo $_SESSION['csrf_token'];
+    // echo $_SESSION['csrf_token'];
 }
 
 ?>
